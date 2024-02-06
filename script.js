dc1c237257063e7f580899c857ca98f70fb9a532
baltazar <arielochefe80@gmail.com>
2024-02-05 21:54:21 -0300

    adicionando js

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const secondHand = document.querySelector('.hand.second-hand');
+const minuteHand = document.querySelector('.hand.min-hand');
+const hourHand = document.querySelector('.hand.hour-hand');
+
+function setDate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    
+}
+setInterval(setDate, 1000);
